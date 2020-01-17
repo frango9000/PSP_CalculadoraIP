62ce15354129b390fa9cd2bcab84bcd8713363a6
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2020-01-17 09:45:17 +0100

    master: Cliente.java

--- src/main/java/psp/a_test/Cliente.java
+++ src/main/java/psp/a_test/Cliente.java
@@ -1,5 +1,40 @@
 package psp.a_test;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
 public class Cliente {
 
+    public static void main(String[] args) {
+        try {
+            System.out.println("Creando socket cliente");
+            Socket clienteSocket = new Socket();
+            System.out.println("Estableciendo la conexi�n");
+
+            InetSocketAddress addr = new InetSocketAddress("192.168.0.1", 5555);
+            clienteSocket.connect(addr);
+
+            InputStream is = clienteSocket.getInputStream();
+            OutputStream os = clienteSocket.getOutputStream();
+
+            System.out.println("Enviando mensaje");
+
+            String mensaje = "mensaje desde el cliente";
+            os.write(mensaje.getBytes());
+
+            System.out.println("Mensaje enviado");
+
+            System.out.println("Cerrando el socket cliente");
+
+            clienteSocket.close();
+
+            System.out.println("Terminado");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
